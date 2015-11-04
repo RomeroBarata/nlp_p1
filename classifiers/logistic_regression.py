68198639c6719c2eb723cd04460429e68c9cd0d3
edumcd <eduardo@Eduardos-MBP.home>
2015-11-04 10:49:30 -0300

    cost_funtion_reg implemented and changes to cost_function
    -I’m adding the bias unit to the training set inside the functions
    cost_function and cost_function_reg
    -I’m also creating the initial theta matrix inside those functions, so
    I could add the theta(0) parameter more easily at cost_function_reg
    -now using the alias “np” for numpy

--- classifiers/logistic_regression.py
+++ classifiers/logistic_regression.py
@@ -1,22 +1,50 @@
 import math
-import numpy
+import numpy as np
 
 def sigmoid(x):
     return 1 / (1 + math.exp(-x))
 
 # Returns the cost J and the gradient vector
-def cost_function(training, theta, classes):
+def cost_function(training, classes):
     m = classes.size
 
-    hypothesis = sigmoid(numpy.dot(training,theta))
+    # add the bias unit to the training set
+    training = np.concatenate(np.ones((training.shape[0],1)),training, axis=1)
 
-    j_1 = numpy.dot(numpy.transpose(classes), math.log(hypothesis))
-    j_2 = numpy.dot(numpy.transpose(numpy.subtract(1,classes)), math.log(numpy.subtract(1,hypothesis)))
-    J = numpy.multiply(1/m, numpy.multiply(-1,numpy.add(j_1, j_2)))
+    theta = np.zeros((training.shape[1],1))
 
-    gradient = numpy.multiply(1/m, numpy.dot(numpy.traspose(numpy.subtract(hypothesis,classes)),training))
+    hypothesis = sigmoid(np.dot(training,theta))
+
+    j_1 = np.dot(np.transpose(classes), math.log(hypothesis))
+    j_2 = np.dot(np.transpose(np.subtract(1,classes)), math.log(np.subtract(1,hypothesis)))
+    J = np.multiply(1/m, np.multiply(-1,np.add(j_1, j_2)))
+
+    gradient = np.multiply(1/m, np.dot(np.traspose(np.subtract(hypothesis,classes)),training))
 
     return (J, gradient)
 
 
-# def cost_function_reg(training, theta, lambda):
+def cost_function_reg(training, classes, lambda):
+    m = classes.size
+
+    # theta2 excludes the first parameter in orther to be reguarized
+    theta2 = np.zeros((training.shape[1],1))
+
+    # add the bias unit to the training set
+    training = np.concatenate(np.ones((training.shape[0],1)),training, axis=1)
+
+    theta = np.zeros((training.shape[1],1))
+
+    hypothesis = sigmoid(np.dot(training,theta))
+
+    j_1 = np.dot(np.transpose(classes), math.log(hypothesis))
+    j_2 = np.dot(np.transpose(np.subtract(1,classes)), math.log(np.subtract(1,hypothesis)))
+    j_3 = np.multiply(lambda/2*m,np.sum(theta2**2))
+    J = np.multiply(1/m, np.multiply(-1,np.add(j_1, j_2))) + j_3
+
+    gradient_1 = np.multiply(1/m, np.dot(np.traspose(np.subtract(hypothesis,classes)),training))
+    gradient_2 = np.multiply(lambda/m, theta)
+    gradient = np.transpose(gradient_1) + gradient_2
+    gradient[0] = np.multiply(1/m, np.dot(np.traspose(np.subtract(hypothesis,classes)),training[:0]))
+
+    return (J, gradient)

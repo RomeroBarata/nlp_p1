0028b0c660720a4d02aeee40f6f7b20988732813
edumcd <eduardo@Eduardos-MBP.home>
2015-11-04 23:13:44 -0300

    added code for testing in the main.py
    -some fixes in the gradient_descent and cost_function
    
    * for some reason gradient_descent is not minimizing the cost

--- classifiers/logistic_regression.py
+++ classifiers/logistic_regression.py
@@ -1,34 +1,33 @@
-import math
 import numpy as np
 
 def sigmoid(x):
-    return 1 / (1 + math.exp(-x))
+    return 1 / (1 + np.exp(-x))
 
 vsigmoid = np.vectorize(sigmoid)
 
 # Returns the cost J and the gradient vector
 def cost_function(training, classes, theta):
-    m = classes.size
+    m = len(classes)
 
     hypothesis = vsigmoid(np.dot(training,theta))
 
-    j_1 = np.dot(np.transpose(classes), math.log(hypothesis))
-    j_2 = np.dot(np.transpose(np.subtract(1,classes)), math.log(np.subtract(1,hypothesis)))
-    J = (1/m) * (-1) * (np.add(j_1, j_2))
-
+    j_1 = np.dot(np.transpose(classes), np.log(hypothesis))
+    j_2 = np.dot(np.transpose(np.subtract(1, classes)), np.log(np.subtract(1,hypothesis)))
+    J = -1/m * (j_1[0][0] + j_2[0][0])
+    
     return J
 
 
 def cost_function_reg(training, classes, regLambda, theta):
-    m = classes.size
+    m = len(classes)
 
     # theta2 excludes the first parameter in orther to be reguarized
     theta2 = theta[range(1,theta.size)]
 
     hypothesis = vsigmoid(np.dot(training,theta))
 
-    j_1 = np.dot(np.transpose(classes), math.log(hypothesis))
-    j_2 = np.dot(np.transpose(np.subtract(1,classes)), math.log(np.subtract(1,hypothesis)))
+    j_1 = np.dot(np.transpose(classes), np.log(hypothesis))
+    j_2 = np.dot(np.transpose(np.subtract(1,classes)), np.log(np.subtract(1,hypothesis)))
     j_3 = (regLambda/2*m) * np.sum(theta2**2)
     J = (1/m) * (-1) * np.add(j_1, j_2) + j_3    
 
@@ -36,21 +35,22 @@ def cost_function_reg(training, classes, regLambda, theta):
 
 
 def gradient_descent(training, classes, theta, alpha, num_iterations):
-    m = classes.size
+    m = len(classes)
 
     for i in range(0,num_iterations):
+        print i
 
         hypothesis = vsigmoid(np.dot(training,theta))
+        
+        gradient = np.multiply(1/m, np.dot(np.transpose(np.subtract(hypothesis, classes)),training))
 
-        gradient = (1/m) * np.dot(np.traspose(hypothesis - classes),training)
-
-        theta = theta - (alpha * gradient)
+        theta = np.subtract(theta, np.transpose(np.multiply(alpha, gradient)))
 
     return theta
 
 
 def gradient_descent_reg(training, classes, theta, alpha, num_iterations, regLambda):
-    m = classes.size
+    m = len(classes)
 
     for i in range(0,num_iterations):
 
--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from classifiers.logistic_regression import *
 
 # Constants
 NUM_MOST_FREQUENT = 200
+TOP_CLASSES = ['earn', 'acquisitions', 'money-fx', 'grain', 'crude', 'trade', 'interest', 'ship', 'wheat', 'corn']
 
 # Extract the fileids of the necessary subset of documents.
-necessary_documents_fileids = reuters.fileids(['earn', 'acquisitions', 'money-fx', 'grain', 'crude', 'trade', 'interest', 'ship', 'wheat', 'corn'])
+necessary_documents_fileids = reuters.fileids(TOP_CLASSES)
 
 # Split into training and testing.
 training_fileids = [w for w in necessary_documents_fileids if w.startswith('training')]
@@ -17,25 +18,33 @@ most_frequent_words = extract_most_frequent_words(training_fileids, NUM_MOST_FRE
 training_featureset = [(document_features(reuters.words(fileid), most_frequent_words), reuters.categories(fileid)) for fileid in training_fileids]
 testing_featureset = [(document_features(reuters.words(fileid), most_frequent_words), reuters.categories(fileid)) for fileid in testing_fileids]
 
-
+print "going to the classifier part"
 
 # Train a classifier.
 
-# training_matrix = [x.values() for (x,y) in training_featureset]
-# TODO: Add the bias unit to the documents matrix
+training_matrix = [x.values() for (x,y) in training_featureset]
+classes_vector = [y for (x,y) in training_featureset]
+n = NUM_MOST_FREQUENT
+m = len(training_matrix)
+# add the bias unit to the training matrix
+training_matrix = np.concatenate((np.ones((m,1)) , training_matrix) , axis=1)
 
-# The initial theta is a vector of zeros of size n
-# theta = np.zeros ((n + 1, 1))
-# thetaReg = np.zeros ((n + 1, 1))
+# creating ten classifiers
+classifiers = dict( (c, np.zeros((n+1,1)) ) for c in TOP_CLASSES )
+
 # regLambda = 1
 
-# Initial cost
-# cost = cost_function(training, classes, theta)
+iterations = 100
+alpha = 0.1
 
-iterations = 1000
-alpha = 0.01
-
-# theta = gradient_descent(training, classes, theta, alpha, iterations)
-# thetaReg = gradient_descent_reg(training, classes, theta, alpha, num_iterations, regLambda)
+c = TOP_CLASSES[0]
+mapClasses = [[1] if c in cl else [-1] for cl in classes_vector]
+print "initial cost"
+cost = cost_function(training_matrix, mapClasses, classifiers[c])
+print cost
+bla = gradient_descent(training_matrix, mapClasses, classifiers[c], alpha, iterations)
+print "trained cost"
+cost2 = cost_function(training_matrix, mapClasses, bla)
+print cost2
 
 # Assess the results.
